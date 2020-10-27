5f0223cada0c6df8366adc002041a9c42682cd6c
YatrikAmrutiya <57680397+YatrikAmrutiya@users.noreply.github.com>
2020-10-27 23:21:13 +0530

    Delete graph.py

--- venv/graph.py
+++
@@ -1,9 +0,0 @@
-
-# Create a dataframe
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-fig = plt.figure()
-fig.suptitle('bold figure suptitle', fontsize=14)
-plt.bar(["one","two","three","four"], [1, 3, 3, 4])
-plt.show()
